0c82d02af810b5a29a005f21de0cc1976bd0fce5
Madina <madinatussupova0820@gmail.com>
2022-08-29 23:39:54 +0600

    add players names

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const title = document.querySelector('h1');
 const btn = document.querySelector('button');
 const player1dice = document.querySelector('.img1');
 const player2dice = document.querySelector('.img2');
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+
+function getPlayerName(num, player) {
+	const playerName = prompt(`${num} player name:`);
+	player.textContent = playerName;
+}
+
+const player1name = getPlayerName('First', player1);
+const player2name = getPlayerName('Second', player2);
 
 const randomNumber = function() {
 	return Math.floor(Math.random() * 6) + 1;
 };
-
 
 btn.addEventListener('click', (e) => {
 	e.preventDefault();
@@ -17,8 +26,8 @@ btn.addEventListener('click', (e) => {
 	player2dice.setAttribute('src', `images/dice${player2score}.png`);
 
 	if(player1score > player2score) {
-		title.textContent =  "Player 1 wins!";
+		title.textContent =  `${player1.textContent} wins!`;
 	} else if(player1score < player2score) {
-	title.textContent = "Player 2 wins!";
+	title.textContent = `${player2.textContent} wins!`;
 	} else title.textContent = "Draw!";
 });
